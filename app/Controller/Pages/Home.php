b8ad83bbbba94b5dd0dce373082e6ac9dbc0d674
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2022-04-28 19:05:55 -0300

    Create Home Controller

---
+++ app/Controller/Pages/Home.php
@@ -0,0 +1,14 @@
+<?php
+    namespace App\Controller\Pages;
+
+    class Home
+    {
+        /**
+         * Método responsável por retornar o conteúdo da home
+         * @return string
+         */
+        public static function getHome(): string
+        {
+            return 'Hello!';
+        }
+    }
