615973b2bd691bec9e033ed1efdb850854350627
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2022-04-28 21:27:10 -0300

    Update Home controller

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -1,14 +1,21 @@
 <?php
     namespace App\Controller\Pages;
+    use App\Utils\View;
 
-    class Home
+    class Home extends Page
     {
         /**
-         * Método responsável por retornar o conteúdo da home
+         * Return the content of home
          * @return string
          */
         public static function getHome(): string
         {
-            return 'Hello!';
+            $content = View::render('pages/home', [
+                'name' => 'Vinicius Silva',
+                'github' => 'https://github.com/josevini',
+                'linkedin' => 'https://www.linkedin.com/in/josevini/'
+            ]);
+
+            return parent::getPage('Home', $content);
         }
     }
