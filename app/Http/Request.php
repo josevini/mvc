33dfd5adb1b0f1a907c1e10e7901f609e34c0e90
Vinicius Silva <ferreiravini468@gmail.com>
2022-04-30 10:38:32 -0300

    Defining methods and attributes of Request class

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -3,5 +3,81 @@
 
     class Request
     {
+        /**
+         * Http Request Method
+         * @var string
+         */
+        private string $httpMethod;
 
+        /**
+         * Page uri
+         * @var string
+        */
+        private string $uri;
+
+        /**
+         * Url params ($_GET)
+         * @var array
+        */
+        private array $queryParams;
+
+        /**
+         * Post vars ($_POST)
+         * @var array
+        */
+        private array $postVars;
+
+        /**
+         * Request header
+         * @var array
+        */
+        private array $headers;
+
+        public function __construct() {
+            $this->queryParams = $_GET ?? [];
+            $this->postVars = $_POST ?? [];
+            $this->headers = getallheaders();
+            $this->httpMethod = $_SERVER['REQUEST_METHOD'] ?? '';
+            $this->uri = $_SERVER['REQUEST_URI'] ?? '';
+        }
+
+        /**
+         * Return http method
+         * @return string
+        */
+        public function getHttpMethod(): string {
+            return $this->httpMethod;
+        }
+
+        /**
+         * Return uri
+         * @return string
+         */
+        public function getUri(): string {
+            return $this->uri;
+        }
+
+        /**
+         * Return query params
+         * @return array
+         */
+        public function getQueryParams(): array {
+            return $this->queryParams;
+        }
+
+        /**
+         * Return post vars
+         * @return array
+         */
+        public function getPostVars(): array {
+            return $this->postVars;
+        }
+
+        /**
+         * Return headers
+         * @return array
+         */
+        public function getHeaders(): array {
+            return $this->headers;
+        }
     }
